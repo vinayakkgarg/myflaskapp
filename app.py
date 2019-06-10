d97cc9eaf93ae8b1a4f6cc56dca69ba6d1f00167
vinayakkgarg <vinayakk.garg99@gmail.com>
2019-06-11 01:15:40 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from data import Articles
+
+# create object of class Flask
+app = Flask(__name__)
+
+Articles = Articles()
+
+# defining routes ie url
+# we generally return a template
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+
+@app.route('/articles/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+
+# debug = True then we don't need to restart server to view changes
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Articel One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ',
+            'author': 'Vinayakk Garg',
+            'create_date': '10-06-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Articel Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'author': 'Vinayakk Garg2',
+            'create_date': '10-06-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Articel Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'author': 'Vinayakk Garg3',
+            'create_date': '10-06-2021'
+        }
+    ]
+    return articles
